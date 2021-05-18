578229ea791a6ce01c4dc6c69d8bd8d0da8dd21a
Anushka Singh <anushka13singh@gmail.com>
2021-05-18 17:25:26 +0800

    cloudinary for user avatar is set

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+require "open-uri"
+
+puts 'destroy old data'
+
+User.destroy_all
+
+6.times do
+  user = User.new(
+    username: Faker::Name.unique.name,
+    email: Faker::Internet.email,
+    address:Faker::Address.full_address,
+    password:Faker::Alphanumeric.alphanumeric(number: 8)
+    )
+  puts "creating user: #{user.username}"
+  fileav = URI.open(Faker::Avatar.image)
+  user.avatar.attach(io: fileav, filename: "#{user.username}.png", content_type: 'image/png')
+  user.save!
+end
