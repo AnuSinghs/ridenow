58085f7e1ec37299d8507095a69d5569c93e7962
Jack Zhang <junbojack@gmail.com>
2021-05-19 15:22:04 +0800

    geocoder for listing ready

--- db/migrate/20210519065737_create_listings.rb
+++ db/migrate/20210519065737_create_listings.rb
@@ -1,8 +1,6 @@
 class CreateListings < ActiveRecord::Migration[6.0]
   def change
     create_table :listings do |t|
-      t.float :lat
-      t.float :long
       t.text :address
       t.string :name
       t.text :description
