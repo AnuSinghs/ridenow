eda3fc19114a8d0fea7ba999c16de93e1c9ef16e
Anushka Singh <anushka13singh@gmail.com>
2021-05-22 14:21:36 +0800

    listing display between start and end

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -6,12 +6,37 @@ class ListingsController < ApplicationController
     @categories = Category.all
     @journey = Journey.new
 
-    sights = Category.first
-    eats = Category.last
+    listing_example #function created below
 
-    @listingeats = Listing.where(category: eats)
-    @listingsights = Listing.where(category: sights)
+    start_end #function created below
 
+    @listingeats = Listing.by_latitude(@start[0][:lat], @end[0][:lat]).by_longitude(@start[0][:lng], @end[0][:lng]).where(category: Category.last)
+    @listingsights = Listing.by_latitude(@start[0][:lat], @end[0][:lat]).by_longitude(@start[0][:lng], @end[0][:lng]).where(category: Category.first)
+
+    listing_markers
+  end
+
+  private
+
+  def start_end
+    @origin = params[:start]
+    @destination = params[:end]
+    @fit_points = [@start[0], @end[0]]
+  end
+
+  def listing_markers
+     @listing_markers = @listings.geocoded.map do |listing|
+      {
+        lat: listing.latitude,
+        lng: listing.longitude,
+        category: listing.category.name,
+        id: listing.id,
+        info_window: render_to_string(partial:"shared/info_window", locals: { listing: listing })
+      }
+    end
+  end
+
+  def listing_example
     katong =
       [{
         lat: 1.307734,
@@ -29,21 +54,6 @@ class ListingsController < ApplicationController
     else
       @start = marina_bay
       @end = katong
-    end
-
-    @origin = params[:start]
-    @destination = params[:end]
-    
-    @fit_points = [@start[0], @end[0]]
-
-    @listing_markers = @listings.geocoded.map do |listing|
-      {
-        lat: listing.latitude,
-        lng: listing.longitude,
-        category: listing.category.name,
-        id: listing.id,
-        info_window: render_to_string(partial:"shared/info_window", locals: { listing: listing })
-      }
     end
   end
 end
