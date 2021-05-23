96f8d386f2bfc8159efdfe5591801ded4b009ca1
Anushka Singh <anushka13singh@gmail.com>
2021-05-23 15:44:58 +0800

    defined listing and itineraries relationship

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,6 +2,7 @@ class Listing < ApplicationRecord
   belongs_to :category, optional: true
   has_many :listing_tags, dependent: :destroy
   has_many :tags, through: :listing_tags
+  has_many :itineraries, dependent: :destroy
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
