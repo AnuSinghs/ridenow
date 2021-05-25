35accd4c9057c69c70e4b427901bf642b1c12c5a
Jack Zhang <junbojack@gmail.com>
2021-05-25 13:51:19 +0800

    edit is good to go, user journey png is displaying

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -11,8 +11,8 @@ class ListingsController < ApplicationController
       redirect_to root_path
     else
       start_end #function created below for finding the coords for start and end
-      listing_markers #function created below for storing the details of listing and sending to javascript
       listing_eats_sights #function created below for storing the eats and sights
+      listing_markers(@listingeats, @listingsights) #function created below for storing the details of listing and sending to javascript
 
       #passing the data to journey controller
       @origin = params[:start]
@@ -30,17 +30,28 @@ class ListingsController < ApplicationController
     @fit_points = [@start, @end]
   end
 
-  def listing_markers
-     @listing_markers = @listings.geocoded.map do |listing|
-      {
-        lat: listing.latitude,
-        lng: listing.longitude,
-        category: listing.category.name,
-        id: listing.id,
-        info_window: render_to_string(partial:"shared/info_window", locals: { listing: listing })
-      }
-    end
+  def listing_markers(eat_markers, sight_markers)
+    @listing_markers = []
+    eat_markers.geocoded.each do |listing|
+    @listing_markers << {
+      lat: listing.latitude,
+      lng: listing.longitude,
+      category: listing.category.name,
+      id: listing.id,
+      info_window: render_to_string(partial:"shared/info_window", locals: { listing: listing })
+    }
   end
+
+  sight_markers.geocoded.each do |listing|
+   @listing_markers << {
+      lat: listing.latitude,
+      lng: listing.longitude,
+      category: listing.category.name,
+      id: listing.id,
+      info_window: render_to_string(partial:"shared/info_window", locals: { listing: listing })
+    }
+  end
+end
 
   def listing_eats_sights
     @listingeats = Listing.where(category: Category.last).by_latitude(@start[0], @end[0]).by_longitude(@start[1], @end[1])
